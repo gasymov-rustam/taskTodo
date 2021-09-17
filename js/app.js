565834941a26fe1fb9f9cdc4d012015d430faa1a
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-09-17 16:02:14 +0300

    last chenges and release

--- js/app.js
+++ js/app.js
@@ -4,10 +4,7 @@ const todosProgress = document.getElementById("todosProgress");
 const todosDelete = document.getElementById("todosDelete");
 const wrapTodoList = document.getElementById("wrapTodoList");
 
-if (!window.localStorage.getItem("todos")) {
-    window.localStorage.setItem("todos", JSON.stringify([]));
-}
-
+if (!window.localStorage.getItem("todos")) window.localStorage.setItem("todos", JSON.stringify([]));
 let todos = JSON.parse(window.localStorage.getItem("todos"));
 
 renderByTodos(todos, todosNew, todosProgress, todosDelete);
@@ -16,15 +13,13 @@ wrapTodoList.addEventListener("click", (e) => {
     const currentBtn = e.target.closest("button");
     if (currentBtn) {
         const action = currentBtn.dataset.action;
-        const todoId = Number(currentBtn.closest(".todoTask").dataset.id);
-        const todoIdx = todos.findIndex((todo) => todo.id === todoId);
+        const todoID = +currentBtn.closest(".todoTask").dataset.id;
+        const todoIDX = todos.findIndex((todo) => todo.id === todoID);
         if (action === "new" || action === "progress") {
-            todos[todoIdx].updatedAt = Date.now();
-            todos[todoIdx].status++;
-        } else {
-            todos.splice(todoIdx, 1);
-        }
-        todos.sort((a, b) => a.title.localeCompare(b.title) || a.createdAt - b.createdAt || a.updatedAt - b.udatedAt);
+            todos[todoIDX].updatedAt = Date.now();
+            todos[todoIDX].status++;
+        } else todos.splice(todoIDX, 1);
+        todos.sort((a, b) => a["title"].localeCompare(b["title"]) || a["createdAt"] - b["createdAt"] || a["updatedAt"] - b["updatedAt"]);
         renderByTodos(todos, todosNew, todosProgress, todosDelete);
         window.localStorage.setItem("todos", JSON.stringify(todos));
     }
@@ -32,11 +27,12 @@ wrapTodoList.addEventListener("click", (e) => {
 
 createFormEl.addEventListener("submit", (e) => {
     e.preventDefault();
-    const newTodo = new Todo(e.target.title.value, e.target.title.value);
+    const newTodo = new Todo(e.target.title.value, e.target.body.value);
     todos.push(newTodo);
-    todos.sort((a, b) => a.title.localeCompare(b.title) || a.createdAt - b.createdAt || a.updatedAt - b.udatedAt);
-    const columnFirst = todos.filter((todo) => todo.status === 0);
-    renderTodos(todosNew, columnFirst);
+    todos.sort((a, b) => a["title"].localeCompare(b["title"]) || a["createdAt"] - b["createdAt"] || a["updatedAt"] - b["updatedAt"]);
+    // todos.sort((a, b) => a.title.localeCompare(b.title) || a.createdAt - b.createdAt || a.updatedAt - b.udatedAt);
+    const colFirst = todos.filter((todo) => todo.status === 0);
+    renderTodos(todosNew, colFirst);
     window.localStorage.setItem("todos", JSON.stringify(todos));
     e.target.reset();
 });
@@ -45,30 +41,30 @@ function renderTodos(elem, array) {
     elem.innerHTML = createTodosHTML(array).join("");
 }
 
-function createTodosHTML(todos) {
-    return todos.map((todo) => createTodoHTML(todo));
+function createTodosHTML(array) {
+    return array.map((todo) => createTodoHTML(todo));
 }
 
 function createTodoHTML(todo) {
     let actionBtn = "";
     switch (todo.status) {
         case 0:
-            actionBtn = `<button data-action="new">Set in progress!</button>`;
+            actionBtn = `<button data-action="new">Set in Progress!</button>`;
             break;
         case 1:
-            actionBtn = `<button data-action="progress">Set in done!</button>`;
+            actionBtn = `<button data-action="progress">Set in Done!</button>`;
             break;
         case 2:
-            actionBtn = `<button data-action="done">Delete!</button>`;
+            actionBtn = `<button data-action="done">Set in Delete!</button>`;
             break;
     }
 
     return `<div class="todoTask ${createTodoStatus(todo.status)}" data-id="${todo.id}">
                 <h2>${todo.title}</h2>
                 ${todo.body ? `<p>${todo.body}</p>` : ""}
-                <p>status: ${createTodoStatus(todo.status)}</p>
-                <time>Created: ${new Date(todo.createdAt).toDateString()}</time>
-                ${todo.updated ? `<time>Updated: ${new Date(todo.updatedAt).toDateString()}</time>` : ""}
+                <p>Status: ${todo.status}</p>
+                <time>CreatedAt: ${Date.now(todo.createdAt).toLocaleString()}</time>
+                ${todo.updatedAt ? `<time>updatedAt: ${Date.now(todo.updateddAt).toLocaleString()}</time>` : ""}
                 ${actionBtn}
             </div>`;
 }
@@ -79,7 +75,7 @@ function Todo(title, body) {
     this.body = body;
     this.status = 0;
     this.createdAt = this.id;
-    this.udatedAt = null;
+    this.updatedAt = null;
 }
 
 function createTodoStatus(status) {
@@ -92,7 +88,7 @@ function createTodoStatus(status) {
             case 2:
                 return "done";
             default:
-                throw "Unknown command";
+                throw "Unknown command!";
         }
     } else {
         switch (status) {
@@ -103,15 +99,15 @@ function createTodoStatus(status) {
             case "done":
                 return 2;
             default:
-                throw "Unknown command";
+                throw "Unknown command!";
         }
     }
 }
 
-function renderByTodos(todos, sectionFirst, sectionSecond, sectionThird) {
-    const columnFirst = todos.filter((todo) => todo.status === 0);
-    const columnSecond = todos.filter((todo) => todo.status === 1);
-    const columnThird = todos.filter((todo) => todo.status === 2);
+function renderByTodos(array, sectionFirst, sectionSecond, sectionThird) {
+    const columnFirst = array.filter((todo) => todo.status === 0);
+    const columnSecond = array.filter((todo) => todo.status === 1);
+    const columnThird = array.filter((todo) => todo.status === 2);
     renderTodos(sectionFirst, columnFirst);
     renderTodos(sectionSecond, columnSecond);
     renderTodos(sectionThird, columnThird);
